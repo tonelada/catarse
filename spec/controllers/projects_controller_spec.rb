1b1be388ffd58f32f949f8837e17ec6d83cbdc65
Diogo Biazus <diogob@gmail.com>
2011-05-30 13:26:19 -0300

    started coding GET index tests

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 describe ProjectsController do
+  describe 'GET index' do
+    before(:each){ get :index, :locale => 'pt' }
+    it{ assigns(:title).should == 'A primeira plataforma de financiamento colaborativo de projetos criativos do Brasil' }
+    it{ assigns(:home_page).should == [] }
+    #@home_page = current_site.present_projects.includes(:user, :category).visible.home_page.limit(6).order('projects_sites."order"').all
+    #@recommended = current_site.present_projects.includes(:user, :category).visible.recommended.not_home_page.not_successful.not_unsuccessful.order('created_at DESC').limit(12).all
+    #@recent = current_site.present_projects.includes(:user, :category).visible.not_home_page.not_recommended.not_successful.not_unsuccessful.order('created_at DESC').limit(12).all
+    #@successful = current_site.present_projects.includes(:user, :category).visible.not_home_page.successful.order('expires_at DESC').limit(12).all
+  end
   it "should confirm backer in moip payment" do
     backer = Factory(:backer, :confirmed => false)
     post :moip, {:id_transacao => backer.key, :status_pagamento => '1', :valor => backer.moip_value}
